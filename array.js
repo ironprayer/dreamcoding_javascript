0b6dd4ab4cf2bd3d46c91351a43be411d7f5b41d
ironprayer <ironprayer1208@gmail.com>
2021-11-21 22:30:00 +0900

    array 2/2

--- array.js
+++ array.js
@@ -28,3 +28,53 @@ for(let fruit of fruits) {
 console.clear();
 // c.for each
 fruits.forEach((value) => {console.log(value)})
+
+// 4. Addtion, deletion, copy
+// push : add an item to the end
+fruits.push('apple', 'orange');
+console.log(fruits);
+
+// pop : remove an item from the end
+fruits.pop()
+fruits.pop()
+console.log(fruits);
+
+// unshift : add an item to the bgiennig
+fruits.unshift('strawbreey', 'remon');
+console.log(fruits);
+
+// shift: remove an item from th bgienning
+fruits.shift();
+fruits.shift();
+console.log(fruits);
+
+// note!! shift, unshift are slower than pop, push
+// splice: remove an item by index position
+fruits.push('straweberry', 'reamon');
+console.log(fruits);
+fruits.splice(1,1);
+fruits.splice(1); // 지울 개수를 패러미터로 넘겨주지 않으면 마지막 인덱스까지 지움
+fruits.splice(1,1,'apple', 'watermelon');
+console.log(fruits);
+
+// combine two arrays
+const fruits2 = ['mango'];
+const newFruits = fruits.concat(fruits2);
+console.log(newFruits);
+
+// 5. Searching
+// find the index
+console.clear();
+console.log(fruits.indexOf('apple'));
+console.log(fruits.indexOf('watermelon'));
+
+// includes
+console.log(fruits.includes('watermelon'));
+console.log(fruits.includes('coconut'));
+
+// lastIndexOf
+console.clear();
+fruits.push('apple');
+console.log(fruits);
+console.log(fruits.indexOf('apple')); // 가장 처음 부합하는 검색 키워드의 부합하는 인덱스
+console.log(fruits.lastIndexOf('apple'));
